2fed45695a7aad6c08bc3676ab5feed1bd42d5a3
momojunzi <mark.holthaus@gmail.com>
2019-02-06 19:51:53 -0800

    added google map

--- client/src/components/directions/directions.js
+++ client/src/components/directions/directions.js
@@ -3,12 +3,27 @@ import SideTab from '../../components/sideTab/sideTab';
 
 class Directions extends Component {
     render() {
+
+        const mapStyle = {
+            border: 0
+        }
         return(
             <div className='container'>
                 <SideTab  name='invite' text='The Event'/>
                 <SideTab  name='rsvp' text='R S V P'/>
                 <SideTab  name='directions' text='Directions'/>
-                <div className='canvas'> Coming soon!</div>
+                <div className='canvas'> 
+                <iframe 
+                    width="100%" 
+                    height="100%" 
+                    frameborder="0" 
+                    style={mapStyle} 
+                    src="https://www.google.com/maps/embed/v1/place?q=32083%20Alvarado-Niles%20Rd%2C%20Union%20City%2C%20CA%2094587&key=AIzaSyAnQTPhx6nkRVy0maCdtgLOIsfPQLHru9k" 
+                    allowfullscreen>
+
+                </iframe>
+                        
+                </div>
             </div>
         )
     }
